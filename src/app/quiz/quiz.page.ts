86e6556f7ad8b82a7401361f0d1802b834186c43
Egor Nesterov <hardsoncore@gmail.com>
2022-01-07 16:19:17 +0200

    added basic functionality to the Quiz page

--- src/app/quiz/quiz.page.ts
+++ src/app/quiz/quiz.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
 
+import { QueryParams } from '../models/app.model';
 import { Question } from '../models/question.model';
 import { QuestionsService } from '../services/questions.service';
 
@@ -10,11 +11,38 @@ import { QuestionsService } from '../services/questions.service';
   styleUrls: ['quiz.page.scss']
 })
 export class QuizPage implements OnInit {
-  question: Observable<Question>;
+  question: Question;
 
-  constructor(private questionsService: QuestionsService) {}
+  constructor(
+    private questionsService: QuestionsService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
-    this.question = this.questionsService.getRandomQuestion();
+    this.getNextQuestion();
+  }
+
+  public visitTheoryPage(): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        canGoBack: true,
+        questionId: this.question.id,
+      } as QueryParams
+    };
+
+    this.router.navigate(['tabs/questions/question-info'], navigationExtras);
+  }
+
+  public submitAnswer(): void {
+
+  }
+
+  public getNextQuestion(): void {
+    this.question = null;
+
+    // with timout looks better
+    setTimeout(() => {
+      this.questionsService.getRandomQuestion().subscribe(q => this.question = q);
+    }, 1000);
   }
 }
