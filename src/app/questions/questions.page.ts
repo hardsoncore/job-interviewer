d8e9219f6b1219d3c868b2e20c3098c712077a45
Egor Nesterov <hardsoncore@gmail.com>
2022-01-08 12:30:02 +0200

    added correctness percent to the Quiz page and Questions page

--- src/app/questions/questions.page.ts
+++ src/app/questions/questions.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionsService } from '../services/questions.service';
 
-import { Question } from '../models/question.model';
+import { Question, Results } from '../models/question.model';
 import { NavigationExtras, Router } from '@angular/router';
 import { QueryParams } from '../models/app.model';
+import { ResultsService } from '../services/results.service';
 
 @Component({
   selector: 'app-questions',
@@ -12,14 +13,21 @@ import { QueryParams } from '../models/app.model';
 })
 export class QuestionsPage implements OnInit {
   questions: Question[] = [];
+  results: Results[];
 
   constructor(
     private questionsService: QuestionsService,
     private router: Router,
+    private resultsService: ResultsService,
   ) { }
 
   ngOnInit() {
     this.questionsService.questions.subscribe(questions => this.questions = questions);
+    this.results = this.resultsService.getResults();
+  }
+
+  public getPercentById(id: number): number {
+    return this.resultsService.getPercentById(id);
   }
 
   public clickOnQuestion(question: Question): void {
