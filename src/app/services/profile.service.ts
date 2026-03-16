d9bfb77aeceb08169bcf494a1e93aadfa94ab87f
y.niestierov <hardsoncore@gmail.com>
2026-03-16 19:47:07 +0100

    feat (avatar) - optimize avatar, remove localstor profile logic - as not needed

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Profile } from '../models/profile.model';
 
@@ -7,35 +7,16 @@ import { Profile } from '../models/profile.model';
   providedIn: 'root'
 })
 export class ProfileService {
-  private _profile: BehaviorSubject<Profile> = new BehaviorSubject({} as Profile);
-
-  constructor() {
-    this._initializeProfile();
-  }
+  constructor() { }
 
   get profile(): Observable<Profile> {
-    return this._profile.asObservable();
-  }
-
-  public setProfile(profile: Profile): void {
-    this._profile.next(profile);
-    localStorage.setItem('profile', JSON.stringify(profile));
-  }
-
-  private _initializeProfile(): void {
-    let profile = JSON.parse(localStorage.getItem('profile'));
-    if (!profile) {
-      profile = this._getDefaultProfile();
-      this.setProfile(profile);
-    }
-
-    this._profile.next(profile);
+    return of(this._getDefaultProfile());
   }
 
   private _getDefaultProfile(): Profile {
     const defaultProfile: Profile = {
       username: 'JS_FOX',
-      avatarUrl: 'assets/fox_js.png',
+      avatarUrl: 'assets/js_fox.jpeg',
     };
 
     return defaultProfile;
